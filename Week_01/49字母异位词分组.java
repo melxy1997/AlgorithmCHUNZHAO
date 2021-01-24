db147770fa0cc5839a26cb6477a6e298b55f8b0e
“MaRui” <marui@qq.com>
2021-01-24 19:49:52 +0800

    中等题我啥也不会

---
+++ Week_01/49字母异位词分组.java
@@ -0,0 +1,16 @@
+class Solution {
+    //map,首先排序，排序之后可以作为hash的键
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<String, List<String>>();
+        for (String str : strs) {
+            char[] array = str.toCharArray();
+            Arrays.sort(array);
+            String key = new String(array);
+            List<String> list = map.getOrDefault(key, new ArrayList<String>());
+            list.add(str);
+            map.put(key, list);
+        }
+        return new ArrayList<List<String>>(map.values());
+    }
+}
+
